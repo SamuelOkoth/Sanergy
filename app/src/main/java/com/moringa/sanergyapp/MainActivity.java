ec2690ba6dd4d5cd810b70fbf06620b1447de127
SamuelOkoth <okothsamuel78@gmail.com>
2019-09-24 17:11:57 +0300

    This commit adds an overflow menu

--- app/src/main/java/com/moringa/sanergyapp/MainActivity.java
+++ app/src/main/java/com/moringa/sanergyapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.moringa.sanergyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
     }
 }
