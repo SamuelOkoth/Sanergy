f14f544a2481d97175d9659164246a22025e5793
Davisdavy <davisdavy96@gmail.com>
2019-09-27 09:34:04 +0300

    This commit add a fragment to admins dashboard

---
+++ app/src/main/java/com/moringa/sanergyapp/ui/EmployeeDetailActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.sanergyapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringa.sanergyapp.R;
+
+public class EmployeeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_detail);
+    }
+}
