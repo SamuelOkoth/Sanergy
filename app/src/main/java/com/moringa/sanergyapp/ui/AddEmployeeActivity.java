1505b4edea183d2b578dd32e7ca3d97d225fccde
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-01 09:00:41 +0300

    This commit saves data in firabase

--- app/src/main/java/com/moringa/sanergyapp/ui/AddEmployeeActivity.java
+++ app/src/main/java/com/moringa/sanergyapp/ui/AddEmployeeActivity.java
@@ -10,7 +10,7 @@ import com.google.firebase.database.core.view.View;
 import com.moringa.sanergyapp.R;
 import com.moringa.sanergyapp.models.Employees;
 
-public class AddEmployeeActivity extends AppCompatActivity {
+public class AddEmployeeActivity extends AppCompatActivity  {
 private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
